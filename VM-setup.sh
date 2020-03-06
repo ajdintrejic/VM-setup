0d97389e3a00255b6be5e725946dc967f19d22d5
Ajdin Trejic <ajdintrejic@protonmail.ch>
2020-03-06 11:29:31 +0100

    Added package installer

--- VM-setup.sh
+++ VM-setup.sh
@@ -9,4 +9,17 @@ echo "Changing the resolution: 1920x1080"
 xrandr --newmode "1920x1080"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode Virtual1 1920x1080
 xrandr --output Virtual1 --mode 1920x1080
+
+#Install gobuster if running Kali
+DISTRO=`uname -r`
+echo "Running: " $DISTRO
+if [[ $DISTRO == *"KALI"* ]]; then	
+	echo "Kali detected"
+	echo "Installing: gobuster"
+	apt-get update  #Get the latest package lists
+	apt-get install gobuster -y
+else
+	echo "Kali not detected, not installing any packages"
+fi
+
 echo "Bye!"
