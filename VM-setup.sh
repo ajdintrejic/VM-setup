7a882c239ddcef2d5ca1095341bba9891c299d9c
Ajdin Trejic <ajdintrejic@protonmail.ch>
2020-03-06 13:21:28 +0100

    added background changer

--- VM-setup.sh
+++ VM-setup.sh
@@ -10,6 +10,9 @@ xrandr --newmode "1920x1080"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -
 xrandr --addmode Virtual1 1920x1080
 xrandr --output Virtual1 --mode 1920x1080
 
+#Changing background
+xsetroot -solid "#000000"
+
 #Install gobuster if running Kali
 DISTRO=`uname -r`
 echo "Running: " $DISTRO
