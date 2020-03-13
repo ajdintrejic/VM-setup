f8cf3d75b93ee92cb8aff1c7f4124554994d2307
Ajdin Trejic <ajdintrejic@protonmail.ch>
2020-03-13 11:21:55 +0100

    Removed the background change, added flameshot

--- VM-setup.sh
+++ VM-setup.sh
@@ -10,17 +10,16 @@ xrandr --newmode "1920x1080"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -
 xrandr --addmode Virtual1 1920x1080
 xrandr --output Virtual1 --mode 1920x1080
 
-#Changing background
-xsetroot -solid "#000000"
-
-#Install gobuster if running Kali
-DISTRO=`uname -r`
+#Install some packages if running Kali
+DISTRO=`uname -a`
 echo "Running: " $DISTRO
-if [[ $DISTRO == *"KALI"* ]]; then	
+if [[ $DISTRO == *"kali"* ]]; then	
 	echo "Kali detected"
 	echo "Installing: gobuster"
 	apt-get update  #Get the latest package lists
 	apt-get install gobuster -y
+	echo "Installing: flameshot"
+	apt-get install flameshot -y
 else
 	echo "Kali not detected, not installing any packages"
 fi
